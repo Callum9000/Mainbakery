0368070617f12b27f757f898ac21e63f6d1e7d2c
templerpadi <templerpadi@gmail.com>
2022-12-02 11:56:03 +0000

    Added Singleton

--- Inventory.java
+++ Inventory.java
@@ -3,8 +3,8 @@ import java.util.ArrayList;
 
 public class Inventory extends Products {
     public static void main(String[] args) {
-
-        List<String> ingredient_list = new ArrayList<String>();
+        Singleton ingredient_list = Singleton.getInstance();
+        ingredient_list = new ArrayList<String>();
         ingredient_list.add("1");
         System.out.println(ingredient_list);
     }
